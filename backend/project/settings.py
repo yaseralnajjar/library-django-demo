9bb6ee603b64f0635b66e62ea0e80ed476f2aeae
Yaser Alnajjar <kasseryasser@windowslive.com>
2020-03-09 20:17:59 +0300

    Add custom user model

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
 #    }
 # }
 
+# Custom User Model
+
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
