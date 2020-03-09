eb8732ea7f6750714993f8f7faea6da133355cf7
Yaser Alnajjar <kasseryasser@windowslive.com>
2020-03-09 20:34:47 +0300

    Cleanup accounts app URLs

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,5 +1,4 @@
-from accounts.views import (UserLoginAPIView, UserRegistrationAPIView,
-                            UserTokenAPIView)
+
 from core import views
 from django.contrib import admin
 from django.urls import include, path
@@ -7,9 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/auth/users/', UserRegistrationAPIView.as_view(), name='list'),
-    path('api/auth/users/login/', UserLoginAPIView.as_view(), name='login'),
-    path('api/auth/tokens/<key>/', UserTokenAPIView.as_view(), name='token'),
+    path('api/auth/', include('accounts.urls')),
 
     path('api/hello/', views.HelloView.as_view(), name='hello'),
 ]
