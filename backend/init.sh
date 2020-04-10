576af87e13b9f12f270f35e032e7fe4ec5903e8a
Yaser Alnajjar <kasseryasser@windowslive.com>
2020-04-10 15:39:40 +0300

    Update django docker init script to support dever

--- backend/init.sh
+++ backend/init.sh
@@ -1,8 +1,13 @@
+# Install required packages
+
 apt update
 apt -y upgrade
 
 apt-get -y install python3-dev libpq-dev postgresql postgresql-contrib
 service postgresql restart
+
+
+# Create db table, user, password, and permissions
 
 su - postgres -c "psql -c \"CREATE DATABASE dbapp;\""
 
@@ -16,3 +21,19 @@ create_user_queries=$"
 "
 
 su - postgres -c "psql -c \"${create_user_queries}\""
+
+
+# Migrate and seed admin
+
+cd /app
+pip install virtualenv
+virtualenv /app/venv
+source /app/venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+echo "from accounts.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+
+
+# Start dev server
+
+python manage.py runserver 0.0.0.0:8000
