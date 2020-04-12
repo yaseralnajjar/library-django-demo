e5d1f39eb15b47232dd46946aa14f957bb1fbd12
Yaser Alnajjar <kasseryasser@windowslive.com>
2020-04-12 14:35:40 +0300

    Distinguish pages (books list &  add book)

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,5 @@
+import bookAdd from '@components/book-add/book-add.js'
+import booksList from '@components/books-list/books-list.js'
 import login from '@components/login/login.js'
 import register from '@components/register/register.js'
 import { Router } from '@core/router.js'
@@ -5,6 +7,9 @@ import { Router } from '@core/router.js'
 const routes = {
   '/login.html': login,
   '/register.html': register,
+  '/register.html': register,
+  '/book-add.html': bookAdd,
+  '/books-list.html': booksList,
 }
 const router = new Router(routes)
 router.init()
